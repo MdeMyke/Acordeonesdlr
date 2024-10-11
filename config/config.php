17f4e44393f3ba1984d62b74416ee3c59bead004
MdeMyke <maicolpina8@gmail.com>
2024-10-11 15:49:54 -0500

    db v1

--- config/config.php
+++ config/config.php
@@ -1,16 +1,15 @@
 <?php
 // config.php
-$host = 'localhost';
-$db = 'veterinariabd';
-$user = 'root'; // Cambia a tu usuario
-$pass = ''; // Cambia a tu contraseña
+$host = 'postgres.railway.internal';
+$db = 'railway';
+$user = 'postgres'; // Usuario de la base de datos
+$pass = 'beetfACACadMkYHiNoymWxxrpaJAVrrv'; // Contraseña de la base de datos
+$port = 5432; // Puerto
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
-
-
